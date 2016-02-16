a0aa03e87a47e8dfa78cddf5e469e54705da7076
Ibrahim Lawal <ibrahim@lawal.me>
2016-02-16 17:04:41 +0100

    Initial Commit
    Update README.md
    
    Corrected name to less than 30 characters
    
    Corrected procfile to use node
    
    Switched to express
    
    Made url https
    
    change pay to subscribe
    
    Specified node version in package.json
    
    Changed app name
    
    Corrected desc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bare-node-backend-for-apps
-A simple, Heroku-deployabble backend that you can use to demo our mobile apps.
+# Inline Subscription Sample
+
+A simple, Heroku-deployabble sample payments subscription page that uses paystack inline and nodejs.
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ index.js
@@ -0,0 +1,45 @@
+// Load Environment variables
+require('dotenv').load();
+
+// paystack module is required to make charge token call
+var paystack = require('paystack')(process.env.PAYSTACK_SECRET_KEY);
+
+// uuid module is required to create a random reference number
+var uuid     = require('node-uuid');
+
+var express =  require('express');
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+  extended: true
+}));
+
+app.post('/create-plan', function(req, res) {
+    var amount          = req.body.amount,
+        interval        = req.body.interval;
+        
+        
+    paystack.plan.create({
+      amount:       amount,      
+      interval:     interval,        
+      name:         '' + (amount/100) + 'naira-' + interval, 
+      description:  '' + (amount/100) + 'NGN ' + interval, 
+      currency:     'NGN'    
+    },function(error, body) {
+        res.send({error:error, body:body});
+    });
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('/*', function(req, res){
+  res.status(404).send('Only a post to /charge is allowed');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
